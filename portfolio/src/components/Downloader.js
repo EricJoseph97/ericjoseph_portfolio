9ce4a1d4b32bdd473651cdd740a147b903934ac5
Eric Pence <eric.pence@asi-ware.com>
2020-02-07 11:31:13 -0500

    Updating portfolio

---
+++ portfolio/src/components/Downloader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+
+export default function Header() {
+
+ return(
+    <div className="container">
+        <div className="box">
+            <input type='file'></input>
+        </div>
+    </div>
+ );
+}
---
+++ portfolio/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../App.css';
+
+export default function Header() {
+
+ return(
+    <div className="header">
+        <h3>Eric J Pence</h3>
+    </div>
+ );
+}
