56be44df0c89da2385a2b83f4cf735928bd17c8e
Eric Pence <eric.pence@asi-ware.com>
2020-02-07 17:00:37 -0500

    Updating Resume

--- src/components/Downloader.js
+++ src/components/Downloader.js
@@ -6,7 +6,8 @@ export default function Header() {
  return(
     <div className="container">
         <div className="box">
-            <input type='file'></input>
+                <iframe className="doc-frame" title="Resume" src="https://docs.google.com/document/d/e/2PACX-1vSy2GLGo1NISLZZU3rw8tTcdk-BnGIBO0vsZGoxovGPoAcgH4tHB0ZcFbM8DGWVLjNYpoCxyi3FhDKY/pub?embedded=true" href="https://docs.google.com/document/d/e/2PACX-1vSy2GLGo1NISLZZU3rw8tTcdk-BnGIBO0vsZGoxovGPoAcgH4tHB0ZcFbM8DGWVLjNYpoCxyi3FhDKY/pub"></iframe>
+ 
         </div>
     </div>
  );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import '../App.css';
+import {LinkedIn, GitHub, Mail} from '@material-ui/icons'
 
 export default function Header() {
+
+const handleMail = (e) => {
+
+    var email = document.querySelector("#Email");
+    email.select();
+    document.execCommand("copy");
+    email.nextElementSibling.style.display = 'inline-block';
+
+    
+}
 
  return(
     <div className="header">
         <h3>Eric J Pence</h3>
+        <div className="icon-list">
+            <a className="icon" href="https://www.linkedin.com/in/eric-pence-25319611b/" target='_blank' rel='noreferrer'><LinkedIn fontSize="large"/></a>
+            <a className="icon" href="https://github.com/EricJoseph97" target='_blank' rel='noreferrer'><GitHub fontSize="large"/></a>
+            <span className="icon" onClick={handleMail}><Mail fontSize="large"/></span>
+            <input id="Email" value="epence97@gmail.com"></input>
+            <span class="email-copied">copied!</span>
+        </div>
+
     </div>
  );
 }
