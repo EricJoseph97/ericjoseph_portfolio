f1eaf27ae74b013640f16b105551f1a970adb757
Eric Pence <eric.pence@asi-ware.com>
2020-02-11 14:44:58 -0500

    Test

--- src/components/Downloader.js
+++ src/components/Downloader.js
@@ -7,10 +7,8 @@ export default function Header() {
  return(
     <div className="container">
         <a href="https://docs.google.com/document/d/e/2PACX-1vSy2GLGo1NISLZZU3rw8tTcdk-BnGIBO0vsZGoxovGPoAcgH4tHB0ZcFbM8DGWVLjNYpoCxyi3FhDKY/pub" download><Link></Link></a>
-        <div className="box">
-                
-                <iframe className="doc-frame" title="Resume" src="https://docs.google.com/document/d/e/2PACX-1vSy2GLGo1NISLZZU3rw8tTcdk-BnGIBO0vsZGoxovGPoAcgH4tHB0ZcFbM8DGWVLjNYpoCxyi3FhDKY/pub?embedded=true" ></iframe>
- 
+        <div className="box">       
+            <iframe className="doc-frame" title="Resume" src="https://docs.google.com/document/d/e/2PACX-1vSy2GLGo1NISLZZU3rw8tTcdk-BnGIBO0vsZGoxovGPoAcgH4tHB0ZcFbM8DGWVLjNYpoCxyi3FhDKY/pub?embedded=true" ></iframe>
         </div>
     </div>
  );
