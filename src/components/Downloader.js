35301c4839253254afe6fac24a01737295ec823a
Eric Pence <eric.pence@asi-ware.com>
2020-02-11 15:46:36 -0500

    Formatting

--- src/components/Downloader.js
+++ src/components/Downloader.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import '../App.css';
-import {Link} from '@material-ui/icons'
+import React from "react";
+import "../App.css";
+import { Link } from "@material-ui/icons";
 
 export default function Header() {
-
- return(
+  return (
     <div className="container">
-        <a href="https://docs.google.com/document/d/e/2PACX-1vSy2GLGo1NISLZZU3rw8tTcdk-BnGIBO0vsZGoxovGPoAcgH4tHB0ZcFbM8DGWVLjNYpoCxyi3FhDKY/pub" download><Link></Link></a>
-        <div className="box">       
-            <iframe className="doc-frame" title="Resume" src="https://docs.google.com/document/d/e/2PACX-1vSy2GLGo1NISLZZU3rw8tTcdk-BnGIBO0vsZGoxovGPoAcgH4tHB0ZcFbM8DGWVLjNYpoCxyi3FhDKY/pub?embedded=true" ></iframe>
-        </div>
+      <a
+        href="https://docs.google.com/document/d/e/2PACX-1vSy2GLGo1NISLZZU3rw8tTcdk-BnGIBO0vsZGoxovGPoAcgH4tHB0ZcFbM8DGWVLjNYpoCxyi3FhDKY/pub"
+        download
+      >
+        <Link></Link>
+      </a>
+      <div className="box">
+        <iframe
+          className="doc-frame"
+          title="Resume"
+          src="https://docs.google.com/document/d/e/2PACX-1vSy2GLGo1NISLZZU3rw8tTcdk-BnGIBO0vsZGoxovGPoAcgH4tHB0ZcFbM8DGWVLjNYpoCxyi3FhDKY/pub?embedded=true"
+        ></iframe>
+      </div>
     </div>
- );
+  );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,50 @@
-import React, {useState} from 'react';
-import '../App.css';
-import {LinkedIn, GitHub, Mail} from '@material-ui/icons'
+import React, { useState } from "react";
+import "../App.css";
+import { LinkedIn, GitHub, Mail } from "@material-ui/icons";
 
 export default function Header() {
+  const [copied, setCopied] = useState(false);
 
-const [copied, setCopied] = useState(false);
-
-const handleMail = (e) => {
+  const handleMail = e => {
     var email = document.querySelector("#Email");
     email.select();
     document.execCommand("copy");
     setCopied(true);
-    setTimeout(() => {setCopied(false)}, 3000);
-}
+    setTimeout(() => {
+      setCopied(false);
+    }, 3000);
+  };
 
- return(
+  return (
     <div className="header">
-        <h3>Eric J Pence</h3>
-        <div className="icon-list">
-            <a className="icon" href="https://www.linkedin.com/in/eric-pence-25319611b/" target='_blank' rel='noopener noreferrer'><LinkedIn fontSize="large"/></a>
-            <a className="icon" href="https://github.com/EricJoseph97" target='_blank' rel='noopener noreferrer'><GitHub fontSize="large"/></a>
-            <span className="icon" onClick={handleMail}><Mail fontSize="large"/></span>
-            <input id="Email" value="epence97@gmail.com" onFocus={handleMail}></input>
-            {copied ? <span class="email-copied">Copied!</span> : ''}
-        </div>
-
+      <h3>Eric J Pence</h3>
+      <div className="icon-list">
+        <a
+          className="icon"
+          href="https://www.linkedin.com/in/eric-pence-25319611b/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <LinkedIn fontSize="large" />
+        </a>
+        <a
+          className="icon"
+          href="https://github.com/EricJoseph97"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <GitHub fontSize="large" />
+        </a>
+        <span className="icon" onClick={handleMail}>
+          <Mail fontSize="large" />
+        </span>
+        <input
+          id="Email"
+          value="epence97@gmail.com"
+          onFocus={handleMail}
+        ></input>
+        {copied ? <span class="email-copied">Copied!</span> : ""}
+      </div>
     </div>
- );
+  );
 }
