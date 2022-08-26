cb0f73351b37b3d46ee41a5621bbc63c5e4cbabd
EricJoseph97 <40366704+EricJoseph97@users.noreply.github.com>
2022-08-25 20:50:09 -0400

    Updating resume
    Updating resume and favicon

--- src/components/Downloader.js
+++ src/components/Downloader.js
@@ -6,31 +6,29 @@ export default function Header() {
   return (
     <div className="container">
 
-      
-      <div className="box">
-      <div className="icon-list absolute bg-dark">
-      <a
-        className="icon"
-        href="https://docs.google.com/document/d/e/2PACX-1vSy2GLGo1NISLZZU3rw8tTcdk-BnGIBO0vsZGoxovGPoAcgH4tHB0ZcFbM8DGWVLjNYpoCxyi3FhDKY/pub"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Link className='light-icon'></Link>
-      </a>
-      <a
-        className="icon"
-        href="Eric's Resume 2020.pdf"
-        download
-      >
-        <GetApp className='light-icon'></GetApp>        
-      </a>
-      </div>
 
+      <div className="box">
+        <div className="icon-list absolute bg-dark">
+          <a
+            className="icon"
+            href="https://docs.google.com/document/d/e/2PACX-1vS2rKdWo_Qzttv4DNaogqRbgFdEm0RU8cDskULk3ZKyhdf0s-0ZrjftDrBsSIO_RiKc7rmrGsSy8Hnq/pub"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Link className='light-icon'></Link>
+          </a>
+          <a
+            className="icon"
+            href="Eric's Resume 2022.pdf"
+            download
+          >
+            <GetApp className='light-icon'></GetApp>
+          </a>
+        </div>
         <iframe
           className="doc-frame"
           title="Resume"
-          src="https://docs.google.com/document/d/e/2PACX-1vSy2GLGo1NISLZZU3rw8tTcdk-BnGIBO0vsZGoxovGPoAcgH4tHB0ZcFbM8DGWVLjNYpoCxyi3FhDKY/pub?embedded=true"
-        ></iframe>
+          src="https://docs.google.com/document/d/e/2PACX-1vS2rKdWo_Qzttv4DNaogqRbgFdEm0RU8cDskULk3ZKyhdf0s-0ZrjftDrBsSIO_RiKc7rmrGsSy8Hnq/pub?embedded=true"></iframe>
       </div>
     </div>
   );
--- src/server.js
+++ src/server.js
@@ -4,8 +4,18 @@ const app = express();
 const publicPath = path.join(__dirname, '..', 'build');
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
    res.sendFile(path.join(publicPath, 'index.html'));
-});app.listen(port, () => {
+});
+app.get('/Resume', (req, res) => {
+   res.sendFile(path.join(publicPath, 'resume.html'));
+});
+app.get('/Gallary', (req, res) => {
+   res.sendFile(path.join(publicPath, 'gallary.html'));
+});
+app.get('/Blog', (req, res) => {
+   res.sendFile(path.join(publicPath, 'blog.html'));
+});
+app.listen(port, () => {
    console.log('Server is up!');
 });
